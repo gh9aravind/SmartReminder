ea31aabe9242ea582c1356470543918f28144700
Aravind <aravindreji24@gmail.com>
2026-08-24 09:56:56 +0530

    Update Jest preset for React Native

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  preset: 'react-native',
+  preset: '@react-native/jest-preset',
   setupFilesAfterEnv: ['./jest.setup.js'],
   transformIgnorePatterns: [
     'node_modules/(?!(react-native|@react-native|@react-navigation|@notifee|@supabase)/)',
