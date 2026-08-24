317d1dc270ba5f1c5c681b5e7b3bb1cdc34e0255
Aravind <aravindreji24@gmail.com>
2026-08-24 10:06:18 +0530

    Update transformIgnorePatterns in jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: '@react-native/jest-preset',
   setupFilesAfterEnv: ['./jest.setup.js'],
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native(?!/jest-preset)|@react-navigation|@notifee|@supabase)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@notifee|@supabase|@testing-library)/)',
   ],
   moduleNameMapper: {
     '^@env$': '<rootDir>/__mocks__/env.js',
